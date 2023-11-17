e6a24f372387835a1e60d3647dd85193c60636f9
VorobievKA <vorobievka@gmail.com>
2023-11-17 15:26:29 +0300

    Third. Use http://127.0.0.1:8080/profile

--- src/main/java/ru/netology/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/ru/netology/springbootdemo/SpringBootDemoApplication.java
@@ -2,17 +2,12 @@ package ru.netology.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.netology.springbootdemo.controller.ProfileController;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-
-		var context = SpringApplication.run(SpringBootDemoApplication.class, args);
-		ProfileController profileController = (ProfileController) context.getBean("profileController");
-		System.out.println(profileController.getProfile());
-
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 }
