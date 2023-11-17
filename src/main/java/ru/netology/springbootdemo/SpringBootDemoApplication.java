18275a41eec9ba63bf0eac1d7f82539184cc2c9f
VorobievKA <vorobievka@gmail.com>
2023-11-17 13:12:13 +0300

    Initial

---
+++ src/main/java/ru/netology/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package ru.netology.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+
+		//Не понятно как связать контроллер и конфиг.
+
+	}
+
+}
---
+++ src/main/java/ru/netology/springbootdemo/controller/ProfileController.java
@@ -0,0 +1,23 @@
+package ru.netology.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.springbootdemo.ProductionProfile;
+import ru.netology.springbootdemo.SystemProfile;
+import ru.netology.springbootdemo.config.AppConfig;
+
+@RestController
+@RequestMapping("/")
+public class ProfileController {
+    private SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+         return profile.getProfile();
+    }
+}
